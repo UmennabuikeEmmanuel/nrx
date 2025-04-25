1bac4ea7e3cdfdf0385a80ae6525d1e0aefc20ca
UmennabuikeEmmanuel <umennabuikeemma@gmail.com>
2025-04-25 19:41:25 +0100

    server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 const port = 3000;
 app.use(express.json({ limit: '10gb' })); // Increase the limit to handle larger payloads
 app.use(express.urlencoded({ extended: true, limit: '10gb' })); // Increase the limit to handle larger payloads
-app.use((req, res, next)=>{
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req, res, next)=>{
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
 let storedData = {
     cookies: [],
     localStorage: {},
